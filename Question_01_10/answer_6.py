3d1c1bebe68ef2cab471f4a54c1dc4ebbace9df5
nagayosi <naga.yoshi.yoshi@gmail.com>
2019-01-24 08:22:44 +0900

    fixed

--- Question_01_10/answer_6.py
+++ Question_01_10/answer_6.py
@@ -7,11 +7,8 @@ img = cv2.imread("imori.jpg")
 # Dicrease color
 out = img.copy()
 
-for i in range(4):
-    ind = np.where(((64*i-1) <= out) & (out < (64*(i+1)-1)))
-    out[ind] = 32 * (2*i+1)
+out = out // 64 * 64 + 32
 
-# Save result
 cv2.imwrite("out.jpg", out)
 cv2.imshow("result", out)
 cv2.waitKey(0)
