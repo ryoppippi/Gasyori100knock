3f6bd4a1f9d5a529329dcca29f7ddbb678beb63e
chatflip <oo.chat.flip@gmail.com>
2024-01-08 01:59:46 +0900

    Rename Tutorial to Gasyori100knockCUDA

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 
-static void readImage(){
+static void readImage() {
 	cv::Mat image = cv::imread("..\\Tutorial\\imori.jpg");
 	cv::imshow("image", image);
 	cv::waitKey(0);
